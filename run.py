3cb0fcef8f67615e325974370e9e196103f77b11
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-05-03 14:43:48 +0000

    add sales data to sales worksheet

--- run.py
+++ run.py
@@ -38,7 +38,9 @@ def get_sales_data():
 
 def validate_data(values):
     """
-
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
     try:
         [int(value) for value in values]
@@ -53,4 +55,16 @@ def validate_data(values):
     return True
 
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet... \n")
+    sales_worsheet = SHEET.worksheet("sales")
+    sales_worsheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
